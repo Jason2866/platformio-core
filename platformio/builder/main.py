c6de2914b6f18714f697d936cdbd43a2ce3eb583
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-05-20 12:55:42 +0200

    Update main.py

--- platformio/builder/main.py
+++ platformio/builder/main.py
@@ -147,12 +147,12 @@ if env.subst("$BUILD_CACHE_DIR"):
 if not int(ARGUMENTS.get("PIOVERBOSE", 0)):
     click.echo("Verbose mode can be enabled via `-v, --verbose` option")
 
-# Dynamically load dependent tools
-if "compiledb" or "compiledbtc" in COMMAND_LINE_TARGETS:
-    env.Tool("compilation_db")
-
 if not os.path.isdir(env.subst("$BUILD_DIR")):
     os.makedirs(env.subst("$BUILD_DIR"))
+
+# Dynamically load dependent tools
+if "compiledb" in COMMAND_LINE_TARGETS:
+    env.Tool("compilation_db")
 
 env.LoadProjectOptions()
 env.LoadPioPlatform()
@@ -194,9 +194,6 @@ if env.get("SIZETOOL") and not (
 
 if "compiledb" in COMMAND_LINE_TARGETS:
     env.Alias("compiledb", env.CompilationDatabase("$COMPILATIONDB_PATH"))
-
-if "compiledbtc" in COMMAND_LINE_TARGETS:
-    env.Alias("compiledbtc", env.CompilationDatabase("$COMPILATIONDB_PATH"))
 
 # Print configured protocols
 env.AddPreAction(
